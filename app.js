7d07908dd9b1b5702f7c90a1b276530a1a1ef5a0
Sudipta <dev2.scwt@gmail.com>
2025-01-21 11:11:26 +0530

    admin table changes

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 //const bodyParser = require('body-parser');
 const cors = require("cors");
 require('dotenv').config();
+const bcryptjs = require('bcryptjs');
 
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/product");
 const imageRoute = require('./routes/images');
+const { Admin } = require("./models");
 // const orderRoutes = require("./routes/order");
 // const cartRoutes = require("./routes/cart");
 
@@ -25,6 +27,49 @@ app.use("/api/images", imageRoute);
 // app.use("/api/cart", cartRoutes);
 
 const PORT = process.env.PORT || 3001;
+
+async function hashPassword(password) {
+    try {
+      const salt = await bcryptjs.genSalt(10); // Generate salt
+      const hashedPassword = await bcryptjs.hash(password, salt); // Hash password
+      console.log('Hashed Password:', hashedPassword);
+      return hashedPassword; // Return hashed password (store in DB)
+    } catch (error) {
+      console.error('Error hashing password:', error);
+      throw error; // Re-throw the error for further handling
+    }
+  }
+
+  const initializeDatabase = async () => {
+    try {
+      await Admin.sync(); // Ensures the table exists
+      const adminCount = await Admin.count();
+  
+      if (adminCount === 0) {
+        const password = "dev2.scwt@gmail.com";
+        const hashedPassword = await hashPassword(password); // Await the hashed password
+  
+        await Admin.create({
+          fname: "Moumita",
+          lname: "Das",
+          email: "dev2.scwt@gmail.com",
+          phone: "6294601754",
+          password: hashedPassword, // Use the awaited hashed password
+          userType: "admin",
+          role: "superadmin",
+        });
+        console.log("Default admin record inserted.");
+      } else {
+        console.log("Admins table already has records.");
+      }
+    } catch (error) {
+      console.error("Error during database initialization:", error);
+    }
+  };
+  
+  initializeDatabase();
+  
+
 
 
 //User.sync({ alter: true })
