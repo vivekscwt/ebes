30228bb3cf62f0a5a2126cdccba393ea493beffb
vivekscwt <vivek@scwebtech.com>
2025-01-29 15:38:28 +0530

    Token expiration time update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -140,7 +140,7 @@ const login = async (req, res) => {
         userId: user.id,
       },
       process.env.JWT_KEY,
-      { expiresIn: "1h" } // Token expiration time
+      { expiresIn: "30d" } // Token expiration time
     );
 
     // Return successful response with the token
