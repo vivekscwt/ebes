ac4558c917a9439329d717ba7fd24767bd4b52d4
Sudipta <dev2.scwt@gmail.com>
2025-01-31 13:42:00 +0530

    table name added in models

--- models/media.js
+++ models/media.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Media',
+    tableName: 'media',
   });
   return Media;
 };
--- models/product.js
+++ models/product.js
@@ -38,6 +38,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Product',
+    tableName: 'products',
   });
   return Product;
 };
